21b952195e405b2882cd6593d633a2dd5c76f417
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-10-05 12:36:53 -0400

    added comments to Tls and Tassert

--- ift3913_tp1/Tassert.java
+++ ift3913_tp1/Tassert.java
@@ -11,33 +11,42 @@ public class Tassert {
 
     private static ArrayList<String> assertExp;
 
-
+    /** Obtenir les expressions de la librairie JUnit
+     * @return une liste d'expressions de la librairie JUnit **/
     public static ArrayList<String> getAssertExp() {
         return assertExp;
     }
 
+    /** Mettre à jour la liste d'expressions de tests de la librairie JUnit **/
     public static void setAssertExp(ArrayList<String> assertExpr) {
        assertExp = assertExpr;
     }
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
     // the idea would be to use a regex to check for either 
     // fail, assertArrayEquals, assertEquals, assertNotEquals, assertFalse, assertNotNull, assertNotSame
     // assertNull, assertSame, assertThat, assertThrows, assertTrue
 
-     int countExp=countExp(args[0].toString());
+        int countExp=countExp(args[0].toString());
 
-     System.out.println("Tassert value : " +countExp);
+        System.out.println("Tassert value : " +countExp);
 
-    
-   }
+    }
 
+    /** Cette méthode est utilisée pour compter le nombre d'expressions permettant de
+     * tester le code
+     * @param filename le nom du fichier dont le nombre de lignes de code avec des tests doit être comptabilisé
+     * @return le nombre de lignes de code avec des expressions de tests et -1 si le fichier n'en contient pas**/
     public static int countExp(String filename) {
+
         populateAssertExp();
 
         int countAssertExp = 0;
+
+        // Lecture du fichier
         try {
+
             File fileJava = new File(filename);
             Scanner myReader = new Scanner(fileJava);
 
@@ -45,6 +54,7 @@ public class Tassert {
                 String data = myReader.nextLine();
                 boolean found = checkAssertExp(data);
 
+                // mettre a jour le compteur si on trouve une expression de test
                 if (found == true) {
                     countAssertExp = countAssertExp + 1;
                 }
@@ -65,6 +75,8 @@ public class Tassert {
 
     }
 
+    /** Cette méthode remplit une liste de type String afin de comparer les lignes de code
+     * avec les expressions de JUnit **/
     public static void populateAssertExp(){
 
         ArrayList<String> tempAssert = new ArrayList<>();
@@ -86,18 +98,24 @@ public class Tassert {
 
     }
 
+    /** Cette méthose sert à vérifier si la ligne passé en paramètre contient une expression de test
+     * @param data - ligne que l'on veut tester
+     * @return boolean - retourne si contient (True) une ligne ou pas (False) **/
     public static boolean checkAssertExp(String data){
 
         ArrayList<String> regexData = getAssertExp();
 
         for (int i=0; i<regexData.size();i++){
-
+            // utilisation des regex (liste qu'on a rempli au préalable) pour trouver si ligne = test ou pas
             String regex= regexData.get(i);
 
             Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(data);
+
+            // trouvé (True) ou pas (False)
             boolean matchFound = matcher.find();
 
+            // vérifier s'il s'agit d'un commentaire
             if(matchFound && !data.contains("//") && !data.contains("*")) {
                 return true;
             }
--- ift3913_tp1/Tls.java
+++ ift3913_tp1/Tls.java
@@ -37,6 +37,7 @@ public class Tls {
     public static String getPackageName() {
         return packageName;
     }
+
     public static void setDataLines(List<String[]> toWriteInCSV) {
         dataLines = toWriteInCSV;
     }
@@ -45,6 +46,9 @@ public class Tls {
         return dataLines;
     }
 
+    /** Cette méthode est au coeur de Tls. Elle va extraire toutes les infos des fichiers, l'isoler
+     * de qui n'est pas important et rajouter tout ce qui est pertient à une liste
+     * @param path - folder/fichier dont l'on veut extraire les données**/
     public static void extractData(Path path) {
         
         try {
@@ -94,10 +98,8 @@ public class Tls {
                 tcmp_data=Integer.parseInt(tloc_data)/Integer.parseInt(tassert_data)+"";
             }
 
-
             String[] row = {file_title, " " + getPackageName(), " " + getClassName(), " " + tloc_data+ " ", tassert_data, " " + tcmp_data};
             toWriteInCSV.add(row);
-            
 
             setDataLines(toWriteInCSV);
 
@@ -107,14 +109,22 @@ public class Tls {
         }
     }
 
+    /** Cette méthode va permettre de trouver certains mots dans une ligne de code
+     * On l'utilise afin de trouver des informations telles que les noms du package, classe sans les parenthèses
+     * @param whatToFind - ce que l'on recherche
+     * @param line - ligne dont on veut véfifier s'il contient l'info de whatToFind
+     * @return ce qu'on le recherche comme le nom de la classe sans infos en trop comme les parenthèses
+     * ou le nom de la classe**/
     public static String findWord(String whatToFind, String line) {
 
         String regex = whatToFind;
 
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(line);
+
         boolean matchFound = matcher.find();
 
+        // we found what we wanted, but now we want to isolate it from irrelevant information
         if (matchFound && !line.contains("//") && !line.contains("*")) {
             line = line.replace(whatToFind, "");
             line = line.replace(" ", "");
@@ -124,6 +134,7 @@ public class Tls {
             return line;
         }
 
+        // found nothing
         return null;
     }
 
@@ -131,13 +142,23 @@ public class Tls {
     // Source of the 3 following functions i.e convertToCSV(), givenDataArray_whenConvertToCSV_thenOutputCreated() 
     // and escapeSpecialCharacters() : https://www.baeldung.com/java-csv with only modification made with file name
     // and static mode changes
-   
+    /** Cette méthode va permettre de préparer chaques éléments de la liste pour l'écriture
+     * au fichier CSV et va poser un délimiteur ","
+     * @param data - lignes que l'on veut rajouter au fichier
+     * @see <a href="https://www.baeldung.com/java-csv">Source of the 3 following functions i.e convertToCSV(), givenDataArray_whenConvertToCSV_thenOutputCreated()
+     *     // and escapeSpecialCharacters() : https://www.baeldung.com/java-csv with only modification made with file name
+     *     // and static mode changes </a>**/
     public static String convertToCSV(String[] data) {
         return Stream.of(data)
                 .map(Tls::escapeSpecialCharacters)
                 .collect(Collectors.joining(","));
     }
 
+    /** Cette méthode va prendre une liste en param et va permettre de déterminer si
+     * c'est une première écriture du fichier et va ainsi procéder à l'écriture
+     * @param dataLines lignes que l'on veut ajouter au fichier
+     * @param dir path du fichier CSV
+     * @see <a href="https://stackoverflow.com/questions/3154488/how-do-i-iterate-through-the-files-in-a-directory-and-its-sub-directories-in-java">...</a>*/
     public static void givenDataArray_whenConvertToCSV_thenOutputCreated(List<String[]> dataLines, Path dir) throws IOException {
         File csvOutputFile = (dir.toFile());
 
@@ -159,8 +180,9 @@ public class Tls {
         
        }
 
-    
-
+    /** Cette méthode va permettre de préparer les données correctement pour une écriture
+     * dans un fichier CSV
+     * @param data - ligne que l'on veut modifier pour l'adapter au fichier CSV**/
     public static String escapeSpecialCharacters(String data) {
         String escapedData = data.replaceAll("\\R", " ");
         if (data.contains(",") || data.contains("\"") || data.contains("'")) {
@@ -170,7 +192,9 @@ public class Tls {
         return escapedData;
     }
 
- // Used https://www.baeldung.com/java-csv for writing to CSV file
+ /** Cette méthode va traverser un folder pour en extraire les fichiers et appliquer les
+  * méthodes nécessaires notamment en cas de fichier de csv ou pas
+  * @see  <a href="https://www.baeldung.com/java-csv"> for writing to CSV file as reference</a>**/
     public static void main(String[] args) throws IOException {
          if (args.length ==1) {
              Files.walk(Path.of(args[0])).forEach(path -> fileOrDirec(path));
@@ -181,7 +205,11 @@ public class Tls {
             }
         }
 
-
+    /** Cette méthode est utilisée lorsqu'on veut un fichier CSV et elle
+     * va utiliser extractData() afin d'extraire les données du fichier et
+     * va passer les infos obtenues à la méthode givenDataArray_whenConvertToCSV_thenOutputCreated
+     * @param file - fichier dont l'on veut les infos
+     * @param dir  - path du fichier csv **/
     public static void fileOrDirecCSV( Path file, Path dir) {
         if (file.toFile().isFile() && file.toString().endsWith(".java")) {
             extractData(file);
@@ -192,10 +220,18 @@ public class Tls {
             }
         }
     }
+
+    /** Cette méthode est utilisée lorsqu'on ne veut pas de fichier CSV et elle
+     * va utiliser extractData() afin d'extraire les données du fichier et en faire des sorties en ligne de commande
+     * @param file - fichier dont l'on veut les infos **/
     public static void fileOrDirec( Path file) {
+        // vérifie si le fichier est un fichier java
         if (file.toFile().isFile() && file.toString().endsWith(".java")) {
+
+            // extrait les données
             extractData(file);
 
+            // rajoute une virgule aux données et print
             for (String[] rowData : dataLines) {
                 String line = String.join(", ", rowData);
                 System.out.println(line);
